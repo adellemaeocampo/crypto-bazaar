c0405992877bac79e4b839cc6e1d60df82c3b9f4
Dinhn17 <nquydinh@yahoo.com>
2024-01-29 21:03:47 +1030

    added function getCollectionRankingByVolume which passes data of API to array nftCollectionsRankingByVolume

--- root/assets/js/script.js
+++ root/assets/js/script.js
@@ -8,6 +8,12 @@ const getNFTByContractURL = "https://api.opensea.io/api/v2/chain/";
 // https://api.opensea.io/api/v2/offers/collection/{collection_slug}/nfts/{identifier}/best
 const getPriceOfNFTURL = "https://api.opensea.io/api/v2/offers/collection/"
 
+// Elements
+nftCollectionRankingEl = document.getElementById("nft");
+
+// Global variables
+var nftCollectionsRankingByVolume = [];
+
 // Object: wallet keep track of NFTs and Coins which are the list of NFTs and coins in the wallet save to localstorage
 var wallet = {
   coins : [],
@@ -97,11 +103,35 @@ function getNFTByContract(chain, address, identifier) {
 
 // 
 function getPriceOfNFT(collection, identifier) {
-    const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': '0c9e93e867e640e081971469d0447097'}};
-    const url = getPriceOfNFTURL + collection + "/nfts/" + identifier + "/best";
+  const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': '0c9e93e867e640e081971469d0447097'}};
+  const url = getPriceOfNFTURL + collection + "/nfts/" + identifier + "/best";
 
-    fetch('url', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  fetch('url', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 }
+
+// getCollectionRankingByVolume return data to global variable nftCollectionsRankingByVolume with
+// contract_address, contract_name, logo_url, items_total, volume_1d, volume_change_1d, market_cap
+function getCollectionRankingByVolume() {
+  const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': 'nRneSF8mGcK2LHkhbPKszsvz'}};
+  const url = 'https://restapi.nftscan.com/api/v2/statistics/ranking/collection?sort_field=volume_1d&sort_direction=desc';
+
+  fetch(url, options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      nftCollectionsRankingByVolume[index].contract_address = element.contract_address;
+      nftCollectionsRankingByVolume[index].contract_name = element.contract_name;
+      nftCollectionsRankingByVolume[index].logo_url = element.logo_url;
+      nftCollectionsRankingByVolume[index].items_total = element.items_total;
+      nftCollectionsRankingByVolume[index].volume_1d = element.volume_1d;
+      nftCollectionsRankingByVolume[index].volume_change_1d = element.volume_change_1d;
+      nftCollectionsRankingByVolume[index].market_cap = element.market_cap;
+    };
+  });
+};
